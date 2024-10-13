a6586a7c67c42f67f3eaf84717452c2e1f44e6f4
larry-xue <azoux0311@gmail.com>
2024-10-13 17:11:25 +0800

    remove tip

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -27,14 +27,6 @@ const SearchResults = ({
   data: SearchResult;
   highlight: string;
 }) => {
-  if (data.results.length === 0) {
-    return (
-      <div className="flex flex-col gap-2">
-        <p>No results found</p>
-      </div>
-    );
-  }
-
   return (
     <div className="flex gap-2 w-full flex-wrap mt-10 justify-center">
       {data.results.map((item, idx) => {
