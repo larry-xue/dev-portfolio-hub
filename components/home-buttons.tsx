8166f52e18b20d73087b0f99b2a1a5056fa3c699
larry-xue <azoux0311@gmail.com>
2024-10-13 17:53:40 +0800

    add modal for adding site

--- components/home-buttons.tsx
+++ components/home-buttons.tsx
@@ -1,11 +1,21 @@
 "use client";
 import { Button } from "@nextui-org/button";
 import { useState } from "react";
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@nextui-org/modal";
+import { Code } from "@nextui-org/code";
+import { Link } from "@nextui-org/link";
 
 import { SparklesIcon, PlusAddIcon } from "@/components/icons";
 
 export default function HomeButtons() {
   const [loading, setLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const getRandom = () => {
     setLoading(true);
@@ -25,12 +35,16 @@ export default function HomeButtons() {
       });
   };
 
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
   return (
     <div className="flex gap-4">
       <Button
         color="success"
         startContent={<PlusAddIcon />}
-        onClick={() => alert("Sorry, this feature is not implemented yet")}
+        onClick={handleOpenModal}
       >
         Add Your Site
       </Button>
@@ -42,6 +56,45 @@ export default function HomeButtons() {
       >
         Random
       </Button>
+
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <ModalContent>
+          {() => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Add Your Site
+              </ModalHeader>
+              <ModalBody>
+                <p>
+                  This feature is still under development. Please{" "}
+                  <Link
+                    showAnchorIcon
+                    color="success"
+                    href="https://larryxue.dev/contact"
+                    target="_blank"
+                  >
+                    contact
+                  </Link>{" "}
+                  me with your information, and I will add it to the list.
+                </p>
+                <Code>
+                  <code># Syntax</code>
+                  <pre>
+                    <code>
+                      name: your name
+                      <br />
+                      href: your link
+                    </code>
+                  </pre>
+                </Code>
+              </ModalBody>
+              <ModalFooter>
+                <Button onPress={() => setIsModalOpen(false)}>Close</Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
