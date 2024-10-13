2d3d2c015b6d9cea2e722f6f29713fe12f3e3672
larry-xue <azoux0311@gmail.com>
2024-10-13 18:25:22 +0800

    lint

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -67,11 +67,7 @@ export const Navbar = () => {
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color="success"
-                href={item.href}
-                size="lg"
-              >
+              <Link color="success" href={item.href} size="lg">
                 {item.label}
               </Link>
             </NavbarMenuItem>
