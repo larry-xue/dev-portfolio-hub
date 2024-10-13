c4c94f72554f05f0ac5cffd1a6baa9c81dd6a7fa
larry-xue <azoux0311@gmail.com>
2024-10-13 17:09:23 +0800

    add loding for list page

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -12,8 +12,10 @@ export default function List() {
   const [listByAlphabet, setListByAlphabet] = useState<Map<string, ListItem[]>>(
     new Map(),
   );
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch("/api/list").then(async (res) => {
       const list = await res.json();
       const listMap = new Map<string, ListItem>();
@@ -34,8 +36,19 @@ export default function List() {
       });
 
       setListByAlphabet(alphabetMap);
+      setLoading(false);
     });
   }, []);
+
+  if (loading) {
+    return <div className="flex items-center justify-center mt-40">
+      <div className="flex flex-col items-center justify-center space-y-4">
+
+        <div className="spinner-border animate-spin h-8 w-8 border-4 border-t-green-500 rounded-full"></div>
+        <p className="text-gray-700 font-medium">Loading...</p>
+      </div>
+    </div>
+  }
 
   return (
     <div className="flex gap-4 flex-wrap">
